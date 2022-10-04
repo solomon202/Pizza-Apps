e3557f1ed1c784022448ec74d9ee352d0b0c54dd
vasy <https://github.com/solomon202/NewsApp>
2022-10-05 02:16:35 +0300

    four

--- app/src/main/java/com/example/foodapp/Activity/IntroActivity.java
+++ app/src/main/java/com/example/foodapp/Activity/IntroActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -14,17 +15,23 @@ import com.example.foodapp.R;
 public class IntroActivity extends AppCompatActivity {
     private ConstraintLayout startBtn;
 
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        startBtn = findViewById(R.id.startBtn);
 
-        startBtn=findViewById(R.id.startBtn);
+
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(IntroActivity.this,MainActivity.class));
-            }
+               startActivity(new Intent(IntroActivity.this,MainActivity.class));
+           }
         });
     }
 }
