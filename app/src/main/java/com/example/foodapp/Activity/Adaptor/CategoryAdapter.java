64e8ebd1d6d70de00e90deb4974fedffca369550
vasy <https://github.com/solomon202/NewsApp>
2022-10-15 01:53:33 +0300

    eight

--- app/src/main/java/com/example/foodapp/Activity/Adaptor/CategoryAdapter.java
+++ app/src/main/java/com/example/foodapp/Activity/Adaptor/CategoryAdapter.java
@@ -1,28 +1,59 @@
 package com.example.foodapp.Activity.Adaptor;
-
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.foodapp.Activity.Domain.CategoryDomain;
+import com.example.foodapp.R;
 
-public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
-    @NonNull
-    @org.jetbrains.annotations.NotNull
+import java.util.ArrayList;
+
+
+public class CategoryAdaptor extends RecyclerView.Adapter<CategoryAdaptor.ViewHolder> {
+   ArrayList<CategoryDomain>categoryDomains;
+    public CategoryAdaptor(ArrayList<CategoryDomain>categoryDomains){
+      this.categoryDomains = categoryDomains;
+}
+
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent,int viewType){
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent,int viewType){
+        View infLate = LayoutInflater.from(parent.getContext()).inflate(R.layout.,parent,false);
+        return new ViewHolder(infLate);
     }
   @Override
-    public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull CategoryAdapter.ViewHolder holder,int position){
-
+    public void onBindViewHolder(@NonNull CategoryAdaptor.ViewHolder holder,int position){
+    holder.categoryName.setText(categoryDomains.get(position).getTitle());
+    String picUrl = "";
+    switch (position){
+        case 0:{
+            picUrl="cat_1";
+            holder.mainLayout.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(),R.drawable.cat_background));
+        }
+    }
   }
   @Override
     public int getItemCount(){
-
         return 0;
   }
-  public class ViewHolder{
 
+  public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView categoryName;
+        ImageView categoryPic;
+        ConstraintLayout mainLayout;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            categoryName = itemView.findViewById(R.id.categoryName);
+            categoryPic=itemView.findViewById(R.id.categoryPic);
+            mainLayout=itemView.findViewById(R.id.mainLayout);
+        }
   }
-}
+ }
