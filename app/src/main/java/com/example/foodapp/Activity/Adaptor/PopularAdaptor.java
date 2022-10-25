65b4a432c75f076b9ed4fbdcd5087e4944ac2a87
vasy <https://github.com/solomon202/NewsApp>
2022-10-26 00:01:53 +0300

    thirteen

--- app/src/main/java/com/example/foodapp/Activity/Adaptor/PopularAdaptor.java
+++ app/src/main/java/com/example/foodapp/Activity/Adaptor/PopularAdaptor.java
@@ -7,83 +7,51 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.example.foodapp.Activity.Domain.CategoryDomain;
 import com.example.foodapp.Activity.Domain.FoodDomain;
 import com.example.foodapp.R;
 
 import java.util.ArrayList;
 
 public class PopularAdaptor extends RecyclerView.Adapter<PopularAdaptor.ViewHolder> {
-    ArrayList<FoodDomain>categoryFood;
-    public PopularAdaptor(ArrayList<FoodDomain>categoryDomains){
-        this.categoryFood = categoryDomains;
+    ArrayList<FoodDomain>popularFood;
+    public PopularAdaptor(ArrayList<FoodDomain>popularFood){
+        this.popularFood = popularFood;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-        View infLate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_catecory,parent,false);
+        View infLate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_popular,parent,false);
         return new ViewHolder(infLate);
     }
     @Override
     public void onBindViewHolder(@NonNull PopularAdaptor.ViewHolder holder,int position) {
-        holder.categoryName.setText(categoryFood.get(position).getTitle());
-        String picUrl = "";
-        switch (position) {
-            case 0: {
-                picUrl = "cat_1";
-                holder.mainLayout.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(), R.drawable.cat_background1));
-                break;
-            }
-            case 1: {
-                picUrl = "cat_2";
-                holder.mainLayout.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(), R.drawable.cat_background2));
-                break;
-            }
+        holder.title.setText(popularFood.get(position).getTitle());
+        holder.fee.setText(String.valueOf(popularFood.get(position).getFee()));
 
-            case 2: {
-                picUrl = "cat_3";
-                holder.mainLayout.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(), R.drawable.cat_background3));
-                break;
-            }
-
-            case 3: {
-                picUrl = "cat_4";
-                holder.mainLayout.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(), R.drawable.cat_background4));
-                break;
-            }
-
-            case 4: {
-                picUrl = "cat_5";
-                holder.mainLayout.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(), R.drawable.cat_background5));
-                break;
-            }
-
-        }
-        int drawableResourceLd = holder.itemView.getContext().getResources().getIdentifier(picUrl,"drawable",holder.itemView.getContext().getPackageName());
+        int drawableResourceId = holder.itemView.getContext().getResources().getIdentifier(popularFood.get(position).getPic(),"drawable",holder.itemView.getContext().getPackageName());
         Glide.with(holder.itemView.getContext())
-                .load(drawableResourceLd)
-                .into(holder.categoryPic);
+                .load(drawableResourceId)
+                .into(holder.pic);
     }
     @Override
     public int getItemCount(){
-        return categoryFood.size();
+        return popularFood.size();
     }
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView categoryName;
-        ImageView categoryPic;
-        ConstraintLayout mainLayout;
+        TextView title,fee;
+        ImageView pic;
+        TextView addBtn;
 
         public ViewHolder(@NonNull View itemView){
             super(itemView);
-            categoryName = itemView.findViewById(R.id.categoryName);
-            categoryPic=itemView.findViewById(R.id.categoryPic);
-            mainLayout=itemView.findViewById(R.id.mainLayout);
+            title=itemView.findViewById(R.id.title);
+            fee=itemView.findViewById(R.id.fee);
+            pic=itemView.findViewById(R.id.pic);
+            addBtn=itemView.findViewById(R.id.addBtn);
         }
     }
 }
--- app/src/main/java/com/example/foodapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/foodapp/Activity/MainActivity.java
@@ -22,7 +22,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         recyclerViewCategoryList();
+        recyclerViewPopular();
+
+
 
     }
 
@@ -45,11 +49,12 @@ public class MainActivity extends AppCompatActivity {
     private void recyclerViewPopular(){
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
         recyclerViewPopularList = findViewById(R.id.recyclerView2);
+        recyclerViewPopularList.setLayoutManager(linearLayoutManager);
 
         ArrayList<FoodDomain>foodList= new ArrayList<>();
-        foodList.add(new FoodDomain("Pepperoni pizza","pizza1","slices pepperoni,mozzerella cheese,fresh oregano,ground black pepper,pizza sauce",9.76));
-        foodList.add(new FoodDomain("Cheese Burger","burger","beef,Gouda Cheese,Special Sauce,Lettuce,tomato",8.79));
-        foodList.add(new FoodDomain("Vegetable pizza","pizza2","olive oil,Vegetable oil,pitted kalamata,cherry tomatoes,fresh aregano,basil",8.50));
+        foodList.add(new FoodDomain("Pepperoni pizza","pizza","slices pepperoni,mozzerella cheese,fresh oregano,ground black pepper,pizza sauce",9.76));
+        foodList.add(new FoodDomain("Cheese Burger","pizza","beef,Gouda Cheese,Special Sauce,Lettuce,tomato",8.79));
+        foodList.add(new FoodDomain("Vegetable pizza","pizza","olive oil,Vegetable oil,pitted kalamata,cherry tomatoes,fresh aregano,basil",8.50));
 
         adapter2=new PopularAdaptor(foodList);
         recyclerViewPopularList.setAdapter(adapter2);
